52cda1932c55e91827e7fee701175aabbfb9a165
Аэлина Карыбаева <aelina@Aelinas-MacBook-Pro.local>
2024-05-20 14:01:29 +0600

    test3

--- app.js
+++ app.js
@@ -35,23 +35,32 @@
 //         phoneResult.textContent="not okay ("
 //     }
 // })
-const mailRegExp=/^(?=.*[@.])[a-zA-Z0-9@.]$/
-const passwordRegExp=/^.{4,16}$/
+// const mailRegExp=/^(?=.*[@.])[a-zA-Z0-9@.]$/
+// const passwordRegExp=/^.{4,16}$/
 
-const mailInput=document.querySelector('.mailInput')
-const passwordInput=document.querySelector('.passwordInput')
-const loginResult=document.querySelector('.loginResult')
-const check=document.querySelector('.Check')
+// const mailInput=document.querySelector('.mailInput')
+// const passwordInput=document.querySelector('.passwordInput')
+// const loginResult=document.querySelector('.loginResult')
+// const check=document.querySelector('.Check')
 
-check.addEventListener('click',()=>{
-    if(mailRegExp.test(mailInput.value)){
-        loginResult.textContent="Вы успешно прошли регистрацию!"
-    } else{
-        loginResult.textContent="Ошибка при вводе электронной почты."
+
+
+const mailRegExp = /^(?=.*[@.])[a-zA-Z0-9@.]+$/
+const passwordRegExp = /^.{4,16}$/
+
+const mailInput = document.querySelector('.mailInput')
+const passwordInput = document.querySelector('.passwordInput')
+const loginResult = document.querySelector('.loginResult')
+const check = document.querySelector('.Check')
+
+check.addEventListener('click', () => {
+    if (mailRegExp.test(mailInput.value)) {
+        if (passwordRegExp.test(passwordInput.value)) {
+            loginResult.textContent = "Вы успешно прошли регистрацию!"
+        } else {
+            loginResult.textContent = "Ошибка при вводе пароля."
+        }
+    } else {
+        loginResult.textContent = "Ошибка при вводе электронной почты."
     }
-    if(passwordRegExp.test(passwordInput.value)){
-    loginResult.textContent="Вы успешно прошли регистрацию!"
-    } else(
-        loginResult.textContent="Ошибка при вводе пароля."
-    )
 })
